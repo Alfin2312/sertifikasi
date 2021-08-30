cd3a727ef571830fe1e6a196521607765839a107
alfin2312 <alfin.mulya1999@gmail.com>
2021-08-30 15:08:21 +0700

    sudah selesai

---
+++ application/views/pages/about/Index.php
@@ -0,0 +1,51 @@
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+  <!-- Content Header (Page header) -->
+  <div class="content-header">
+    <div class="container-fluid">
+      <div class="row mb-2">
+        <div class="col-sm-6">
+          <h1 class="m-0">About</h1>
+        </div><!-- /.col -->
+        <div class="col-sm-6">
+          <ol class="breadcrumb float-sm-right">
+            <li class="breadcrumb-item"><a href="#">Home</a></li>
+            <li class="breadcrumb-item active"><?= $pageTitle ?></li>
+          </ol>
+        </div><!-- /.col -->
+      </div><!-- /.row -->
+    </div><!-- /.container-fluid -->
+  </div>
+  <!-- /.content-header -->
+
+  <!-- Main content -->
+  <section class="content">
+    <div class="container-fluid">
+      <!-- Small boxes (Stat box) -->
+      <div class="row">
+        <div class="col-md-12">
+          <!-- Profile Image -->
+          <div class="card card-primary card-outline">
+            <div class="card-body box-profile">
+              <div class="text-center">
+                <img class="profile-user-img img-fluid img-circle" src="<?= base_url("assets/dist/img/foto.jpeg") ?>" alt="User profile picture">
+              </div>
+
+              <h3 class="profile-username text-center">Alfin Mulya Adi Pratama</h3>
+
+              <p class="text-center">1831710037</p>
+
+              <ul class="list-group list-group-unbordered mb-3">
+              <li class="list-group-item">
+                    <b>Tanggal Pembuatan</b> <span class="float-right">29 Agustus 2021</span>
+                  </li>
+              </ul>
+            </div>
+            <!-- /.card-body -->
+          </div>
+          <!-- /.card -->
+        </div>
+    </div><!-- /.container-fluid -->
+  </section>
+  <!-- /.content -->
+</div>
